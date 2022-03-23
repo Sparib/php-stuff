343c737008bd48f7ed78550178eb2265157bc3b0
Sparib <sparibtheonly@gmail.com>
2022-03-23 01:27:41 -0400

    More reflection testing

--- src/pages/test.php
+++ src/pages/test.php
@@ -1,10 +1,17 @@
 <?php
 
-$throwable = function (InvalidArgumentException $e) {
+function throwable(InvalidArgumentException $e) {
     return "a";
 };
 
-echo (new ReflectionFunction($throwable))->getParameters()[0]->getType();
+function catche(Throwable $e) {
+    echo (new ReflectionFunction("throwable"))->getParameters()[0]->getType()->getName() == get_class($e) ? "True" : "False";
+    die();
+};
+
+set_exception_handler("catche");
+
+throw new InvalidArgumentException();
 
 
 ?>
