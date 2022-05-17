34fed80b571d9112ac597508382228c90473f5fb
Sparib <sparibtheonly@gmail.com>
2022-05-17 09:52:28 -0400

    Added beginning of FileHandler

--- src/app/Handlers/FileHandler.php
+++ src/app/Handlers/FileHandler.php
@@ -7,7 +7,7 @@ namespace app\Handlers;
 use app\Internal\Director;
 
 class FileHandler {
-    public readonly array $director;
+    public readonly Director $director;
 
     public function create_director() {
         include_once __BASE_URL__ . "/app/Internal/Director.php";
@@ -15,7 +15,27 @@ class FileHandler {
     }
 
     public function include_files() {
-        # code...
+        $has_error = false;
+        foreach ($this->director->loads as $path) {
+            $origin = $path;
+            if (!str_starts_with($path, "/")) $path = "/${path}";
+            if (!str_starts_with($path, "/app")) $path = "/app${path}";
+            $path = __BASE_URL__ . $path . ".php";
+            if (!is_file($path)) {
+                \Sentry\addBreadcrumb(
+                    new \Sentry\Breadcrumb(
+                        \Sentry\Breadcrumb::LEVEL_WARNING,
+                        \Sentry\Breadcrumb::TYPE_DEFAULT,
+                        'loads',
+                        "Load path does not point to file.",
+                        ['original' => $origin, 'full' => $path]
+                    )
+                );
+                $has_error = true;
+            }
+        }
+
+        if ($has_error) ErrorHandler::nonbreaking("One or more nonbreaking errors encountered during file loading.", \Sentry\Severity::warning());
     }
 }
 
