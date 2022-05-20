06d903ea816d164fd56aafab92744800ff10bd92
Sparib <sparibtheonly@gmail.com>
2022-05-19 22:34:37 -0400

    Created basis for todo page but now I don't feel like doing anything :/

--- src/pages/index.php
+++ src/pages/index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hi</title>
+    <title>Current TODOs</title>
+    <link rel="stylesheet" href="/public/css/index.css">
+    <script src="https://kit.fontawesome.com/ecec20af89.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <p>Balls</p>
+    <ol class="fa-ul">
+        <li><span class="fa-li"><i class="fa-solid fa-spinner fa-pulse"></i></span>Loading</li>
+    </ol>
 </body>
 </html>
