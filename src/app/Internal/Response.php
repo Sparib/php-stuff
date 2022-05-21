1fda74723bd0cfac0789e6ef2c7701a0214e3aea
Sparib <sparibtheonly@gmail.com>
2022-05-21 16:17:20 -0400

    Added success key to response func

--- src/app/Internal/Response.php
+++ src/app/Internal/Response.php
@@ -6,6 +6,7 @@ class Response {
     public static function return_json($array, $code = 200): never {
         header("Content-Type: application/json");
         http_response_code($code);
+        if (!array_key_exists("success", $array)) $array["success"] = true;
         echo json_encode($array);
         exit();
     }
