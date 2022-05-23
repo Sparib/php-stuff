ed25dddd49975bdf8f004d658d04b4dc8218439d
Sparib <sparibtheonly@gmail.com>
2022-05-23 08:33:37 -0400

    Testing

--- src/pages/other.php
+++ src/pages/other.php
@@ -1,4 +1,26 @@
 <?php
 
-echo $_GET["thing"];
-return "hi";
+use app\Internal\Director;
+
+$thing = array(new Director(), 'get_todo');
+echo get_class($thing[0]), '<br>';
+if (is_array($thing))
+    if (is_string($thing[0]))
+        $stringed = "$thing[0]::$thing[1]";
+    else
+        $stringed = get_class($thing[0]) . "->$thing[1]";
+else
+    $stringed = $thing;
+
+echo is_callable($thing), "<br>", $stringed;
+?>
+
+<html>
+<head>
+    <style>
+        * {
+            font-family: monospace;
+        }
+    </style>
+</head>
+</html>
