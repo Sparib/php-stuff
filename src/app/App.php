c33593e495fc788c5c3dffdc351268db5be4d888
Sparib <sparibtheonly@gmail.com>
2022-05-23 10:18:53 -0400

    Added transaction shtuff

--- src/app/App.php
+++ src/app/App.php
@@ -15,6 +15,7 @@ class App {
 
     private readonly FileHandler $file_handler;
     private readonly ErrorHandler $error_handler;
+    private static $parentSpan = null;
 
     public function run() {
         $this->setup_error_handler();
@@ -24,7 +25,36 @@ class App {
         $this->get_page();
     }
 
+    public static function getNewTransactionSpan($op, $desc = null) {
+        App::$parentSpan = \Sentry\SentrySdk::getCurrentHub()->getSpan();
+        $span = null;
+
+        // Check if we have a parent span (this is the case if we started a transaction earlier)
+        if (App::$parentSpan !== null) {
+            $context = new \Sentry\Tracing\SpanContext();
+            $context->setOp($op);
+            if ($desc != null) $context->setDescription($desc);
+            $span = App::$parentSpan->startChild($context);
+
+            // Set the current span to the span we just started
+            \Sentry\SentrySdk::getCurrentHub()->setSpan($span);
+        }
+
+        return $span;
+    }
+
+    public static function completeTransation(\Sentry\Tracing\Span $span) {
+        if ($span !== null) {
+            $span->finish();
+    
+            // Restore the current span back to the parent span
+            \Sentry\SentrySdk::getCurrentHub()->setSpan(App::$parentSpan);
+        }
+    }
+
     private function setup_error_handler() {
+        $span = App::getNewTransactionSpan("error_handler.setup");
+
         include_once __BASE_URL__ . "/app/Handlers/ErrorHandler.php";
         $this->error_handler = new ErrorHandler();
         set_exception_handler(array($this->error_handler, "global_handler"));
@@ -35,13 +65,25 @@ class App {
             include_once __BASE_URL__ . "/pages/error.php";
             return true;
         });
+
+        App::completeTransation($span);
     }
 
     private function setup_file_handler() {
+        $span = App::getNewTransactionSpan("file_handler.setup");
+
         include_once __BASE_URL__ . "/app/Handlers/FileHandler.php";
         $this->file_handler = new FileHandler();
+
+        $dirSpan = App::getNewTransactionSpan("file_handler.director");
         $this->director = $this->file_handler->create_director();
+        App::completeTransation($dirSpan);
+
+        $includeSpan = App::getNewTransactionSpan("file_handler.include");
         $this->file_handler->include_files();
+        App::completeTransation($includeSpan);
+
+        App::completeTransation($span);
     }
 
     private function get_page() {
--- src/server.php
+++ src/server.php
@@ -7,7 +7,10 @@ use app\App;
 
 define("__BASE_URL__", __DIR__);
 
-\Sentry\init(['dsn' => "https://b00795da98a84a5e83ba3fca75f54ed5@sentry.morgverd.com/4"]);
+\Sentry\init([
+    'dsn' => "https://b00795da98a84a5e83ba3fca75f54ed5@sentry.morgverd.com/4",
+    'traces_sample_rate' => 1.0
+]);
 
 if ($_SERVER["REQUEST_URI"] == "/test") {
     include __BASE_URL__ . "/pages/test.php";
@@ -22,4 +25,16 @@ if (!function_exists('app')) {
     }
 }
 
+// Setup full transaction context
+$transactionContext = new \Sentry\Tracing\TransactionContext();
+$transactionContext->setName('Page Request');
+$transactionContext->setOp('http.request');
+
+$transaction = \Sentry\startTransaction($transactionContext);
+
+\Sentry\SentrySdk::getCurrentHub()->setSpan($transaction);
+
 app()->run();
+
+\Sentry\SentrySdk::getCurrentHub()->setSpan($transaction);
+$transaction->finish();
