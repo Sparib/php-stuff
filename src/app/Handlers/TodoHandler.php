bfaf79a6e06750476ac1b6cfa3df13d99041c585
Sparib <sparibtheonly@gmail.com>
2022-07-04 02:40:16 -0400

    More todos

--- src/app/Handlers/TodoHandler.php
+++ src/app/Handlers/TodoHandler.php
@@ -13,6 +13,7 @@ class TodoHandler {
                 new Todo("Also set the cross-origin header", true),
                 new Todo("Config files?", true),
                 new Todo("Dynamic api paths", true),
+                new Todo("Add sql thing"),
                 new Todo("Rewrite flask as php")
             ];
         }
