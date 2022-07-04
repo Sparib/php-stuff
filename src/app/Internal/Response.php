7d19e0b6747211e0f831b011182bb404b73b104d
Sparib <sparibtheonly@gmail.com>
2022-07-04 02:24:10 -0400

    Why was exit not there, excuse me

--- src/app/Internal/Response.php
+++ src/app/Internal/Response.php
@@ -2,12 +2,15 @@
 
 namespace app\Internal;
 
+use JetBrains\PhpStorm\NoReturn;
+
 class Response {
-    public static function return_json($array, $code = 200){
+    public static function return_json($array, $code = 200) : NoReturn {
         header("Content-Type: application/json");
         http_response_code($code);
         if (!array_key_exists("success", $array)) $array["success"] = true;
         echo json_encode($array);
+        exit($code);
     }
 }
 
